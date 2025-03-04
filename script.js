43b4b3a69b4e90996d638bb6b9b48e300615a1e1
camila.moreira <camilammoreira1@gmail.com>
2025-03-04 17:12:11 -0300

    uploading files, check answer

---
+++ script.js
@@ -0,0 +1,19 @@
+const correctAnsewers = ["B", "B", "B", "B"];
+const form = document.querySelector(".quiz-form");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    // Check answers
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnsewers[index]) {
+            score += 25;
+        }
+
+    });
+
+    console.log(score);
+});
